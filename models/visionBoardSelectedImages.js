e688566b667f5dfaa54f646a470b267e0f04ec6e
atiftariq786 <atiftariq786@hotmail.com>
2022-06-30 18:44:58 -0700

    Update api-route with visionBoard post/get/delete code

---
+++ models/visionBoardSelectedImages.js
@@ -0,0 +1,35 @@
+// Dependencies
+// =============================================================
+
+// This may be confusing but here Sequelize (capital) references the standard library
+const Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+const sequelize = require("../config/connection");
+
+// Creates a "Chirp" model that matches up with DB
+let VisionBoardSelectedImages = sequelize.define("selectedImages", {
+  id: {
+    defaultValue: Sequelize.INTEGER,
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  imageId: Sequelize.INTEGER,
+  userId: Sequelize.INTEGER,
+  url: Sequelize.STRING,
+
+  createdAt: {
+    type: Sequelize.DataTypes.DATE,
+    defaultValue: Sequelize.literal(`NOW()`),
+  },
+  updatedAt: {
+    type: Sequelize.DataTypes.DATE,
+    defaultValue: Sequelize.literal(`NOW()`),
+  },
+});
+
+// Syncs with DB
+VisionBoardSelectedImages.sync();
+
+module.exports = VisionBoardSelectedImages;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,15 @@
 let Images = require("../models/visionBoardImages");
+let SelectedImages = require("../models/visionBoardSelectedImages");
 
 module.exports = function (app) {
-  // Get all Images
+  // ====================================VisionBoard Images and Quotes List Routes========================================
   app.get("/api/images/:type", function (req, res) {
     console.log("getting call");
     Images.findAll({
       where: {
         type: req.params.type,
       },
+      order: [["createdAt", "DESC"]],
     }).then(function (results) {
       // results are available to us inside the .then
       res.json(results);
@@ -34,6 +36,54 @@ module.exports = function (app) {
     Images.destroy({
       where: {
         id: req.params.id,
+      },
+    }).then(function () {
+      res.end();
+    });
+  });
+  // ====================================VisionBoard selected images saving through these Routes========================================
+  app.post("/api/visionBoard/save", function (req, res) {
+    console.log("Image Data:");
+    console.log(req.body);
+    SelectedImages.create({
+      id: req.body.id,
+      imageId: req.body.imageId,
+      userId: 0,
+      url: req.body.url,
+    }).then(function (results) {
+      res.json(results);
+      console.log(results, "Selected Images Output");
+    });
+  });
+
+  app.get("/api/visionBoard/me", function (req, res) {
+    console.log("getting call visionboard");
+    SelectedImages.findAll({
+      where: {
+        userId: 0,
+      },
+      order: [["createdAt", "DESC"]],
+    }).then(function (results) {
+      console.log("then");
+      const imageIds = results.map((result) => result.imageId);
+      console.log({ imageIds });
+      return Images.findAll({
+        where: {
+          id: imageIds,
+        },
+      }).then(function (results) {
+        // results are available to us inside the .then
+        res.json(results);
+      });
+    });
+  });
+
+  app.delete("/api/visionBoard/:id", function (req, res) {
+    console.log("Visionboard selected Image ID:");
+    console.log(req.params.id);
+    SelectedImages.destroy({
+      where: {
+        imageId: req.params.id,
       },
     }).then(function () {
       res.end();
