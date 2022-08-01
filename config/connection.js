8f8bc27a91f52cd3dc9a9ac659f3f23f7d57b557
atiftariq786 <atiftariq786@hotmail.com>
2022-08-01 08:48:33 -0700

    update config/connection

--- config/connection.js
+++ config/connection.js
@@ -4,18 +4,31 @@
 
 // Dependencies
 var Sequelize = require("sequelize");
+var config = require("./config.json");
+
+const env = process.env.NODE_ENV;
+let configToUse = config.development;
+
+if (env === "production") {
+  let configToUse = config.production;
+}
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("vp-app", "root", "password", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
-});
+var sequelize = new Sequelize(
+  configToUse.database,
+  configToUse.username,
+  configToUse.password,
+  {
+    host: configToUse.host,
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+    },
+  }
+);
 
 // Exports the connection for other files to use
 module.exports = sequelize;
