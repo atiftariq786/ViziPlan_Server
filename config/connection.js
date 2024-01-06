525644ca0d9383941b23427aa3d8d74f38c3b622
Qandeel Sajid <qandeel.sajid@fictiv.com>
2024-01-05 16:26:40 -0800

    update db

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,14 @@
 // *********************************************************************************
 
 // Dependencies
-var Sequelize = require("sequelize");
-var config = require("./config.json");
+var Sequelize = require('sequelize')
+var config = require('./config.json')
 
-const env = process.env.NODE_ENV;
-let configToUse = config.development;
+const env = process.env.NODE_ENV
+let configToUse = config.development
 
-if (env === "production") {
-  configToUse = config.production;
+if (env === 'production') {
+  configToUse = config.production
 }
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
@@ -21,14 +21,14 @@ var sequelize = new Sequelize(
   {
     host: configToUse.host,
     port: 3306,
-    dialect: "mysql",
+    dialect: 'postgres',
     pool: {
       max: 5,
       min: 0,
       idle: 10000,
     },
   }
-);
+)
 
 // Exports the connection for other files to use
-module.exports = sequelize;
+module.exports = sequelize
